d81db573ae64709be9363c061fa74e1d1e749548
Maximilian Weber <weber.maximilian@googlemail.com>
2011-03-23 12:25:02 +0100

    Updated to Clojure 1.2. swank-clojure has been removed as dev-dependency, because it is available as Leiningen plugin in the meanwhile, it does not need to be specified as a separate dependency any more.

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
 (defproject rabbitcj "0.1.0-SNAPSHOT"
   :description "rabbitmq for clojure."
   :url "http://github.com/clj-sys/rabbitcj"
-  :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.1.0"]
-                 [com.rabbitmq/amqp-client "1.8.1"]]
-  :dev-dependencies [[swank-clojure "1.2.1"]])
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [com.rabbitmq/amqp-client "2.3.1"]])
